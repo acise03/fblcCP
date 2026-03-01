814ab9975199c8ee1e2b4a97af3af80719e66ab9
acise314 <puyuhuan1@gmail.com>
2026-02-28 22:39:28 -0500

    tabs

--- components/haptic-tab.tsx
+++ components/haptic-tab.tsx
@@ -1,18 +1,51 @@
 import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
 import { PlatformPressable } from '@react-navigation/elements';
 import * as Haptics from 'expo-haptics';
+import React, { useRef } from "react";
+import { Animated, View } from "react-native";
 
 export function HapticTab(props: BottomTabBarButtonProps) {
+  const ripple = useRef(new Animated.Value(0)).current;
+  const handlePressIn = (ev: any) => {
+    Animated.timing(ripple, {
+      toValue: 1,
+      duration: 200,
+      useNativeDriver: true,
+    }).start(() => {
+      ripple.setValue(0);
+    });
+
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    props.onPressIn?.(ev);
+  };
+
   return (
-    <PlatformPressable
-      {...props}
-      onPressIn={(ev) => {
-        if (process.env.EXPO_OS === 'ios') {
-          // Add a soft haptic feedback when pressing down on the tabs.
-          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-        }
-        props.onPressIn?.(ev);
-      }}
-    />
+    <PlatformPressable {...props} onPressIn={handlePressIn} style={{ flex: 1 }}>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Animated.View
+          style={{
+            position: "absolute",
+            width: 45,
+            height: 45,
+            borderRadius: 22,
+            backgroundColor: "rgba(0,0,0,0.12)",
+            transform: [
+              {
+                scale: ripple.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0.2, 1.3],
+                }),
+              },
+            ],
+            opacity: ripple.interpolate({
+              inputRange: [0, 1],
+              outputRange: [0, 0.25],
+            }),
+          }}
+        />
+
+        {props.children}
+      </View>
+    </PlatformPressable>
   );
 }
