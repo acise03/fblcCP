9f9bcc7d6578a73896ded44411d4412c4fd773a7
Penguin60 <radean.rashed@gmail.com>
2026-02-28 21:51:28 -0500

    Fix profile menu

--- app/components/modalSettings.tsx
+++ app/components/modalSettings.tsx
@@ -1,8 +1,7 @@
 import { useAuthStore } from "@/store/useAuthStore";
 import { useModalSettingsStore } from "@/store/useModalSettingsStore";
-import Feather from "@expo/vector-icons/Feather";
 import { Link, useRouter } from "expo-router";
-import { Image, Modal, Pressable, Text, View } from "react-native";
+import { Modal, Pressable, Text, View } from "react-native";
 import "../../global.css";
 
 export default function ModalSettings() {
@@ -14,40 +13,19 @@ export default function ModalSettings() {
   const isBusiness = useAuthStore((state) => state.ownedBusiness !== null);
 
   return (
-    <Modal transparent visible={modalVisible} animationType="slide">
+    <Modal transparent visible={modalVisible} animationType="none">
       <Pressable className="h-full w-full" onPress={() => setVisible(false)}>
-        <View className="flex justify-center items-center h-full">
+        <View className="flex items-end pt-24 pr-8 h-full">
           <View
-            className="flex w-2/3 h-[22.5rem] shadow-md bg-[#FFF8F0] rounded-2xl p-4 items-center"
+            className="flex w-56 shadow-md bg-[#FFF8F0] rounded-2xl p-4 items-center"
             onStartShouldSetResponder={() => true}
           >
-            <View className="flex flex-row justify-between items-center w-full">
-              <Pressable onPress={() => setVisible(false)}>
-                <Feather name="x" size={24} color="gray" />
-              </Pressable>
-              <Text className="text-lg" style={{ fontFamily: "Rubik" }}>
-                Menu
-              </Text>
-              <Image
-                resizeMode="contain"
-                className="rounded-full w-10 h-10 bg-gray-500"
-              />
-            </View>
             <Link
               href="/accountSettings"
-              className="my-4"
+              className="my-3 font-medium"
               onPress={() => setVisible(false)}
             >
               Account Settings
-            </Link>
-            <Link href="/" className="my-4">
-              Preferences
-            </Link>
-            <Link href="/" className="my-4">
-              Theme
-            </Link>
-            <Link href="/" className="mt-4 mb-6">
-              Language
             </Link>
             <Pressable
               onPress={() => {
@@ -59,9 +37,10 @@ export default function ModalSettings() {
                     : router.navigate("/(business)")
                   : router.navigate("/(customer)/createBusiness");
               }}
-              className="flex-1 w-full bg-purple-100 justify-center items-center rounded-xl"
+              className="w-full mt-3 justify-center items-center rounded-xl py-3"
+              style={{ backgroundColor: "rgba(255, 182, 39, 0.6)" }}
             >
-              <Text>
+              <Text className="font-medium">
                 {isBusiness
                   ? modalMode === "business"
                     ? "Switch to Customer"
